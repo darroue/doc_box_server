e360af2bb4c57872af934565ebcc786a07ce2b00
Petr Radouš <darroue@gmail.com>
2024-02-29 11:59:51 +0100

    add QrCode::PaymentService

---
+++ services/qr_code/payment_service.rb
@@ -0,0 +1,23 @@
+require "rqrcode"
+
+module QrCode
+  class PaymentService
+    def call(iban:, variable_symbol:, specific_symbol:, amount:, message:, currency:, payment_date:)
+      data = {
+        ACC: iban,
+        "X-VS": variable_symbol,
+        "X-SS": specific_symbol,
+        AM: amount,
+        MSG: message,
+        DT: payment_date,
+        CC: currency
+      }.reject(& -> (k,v) { v.nil? || v.strip == "" })
+
+      qrcode = RQRCode::QRCode.new([
+        'SPD',
+        '1.0',
+        data.map(& ->(k, v) { "#{k}:#{v}" })
+      ].flatten.join('*'))
+    end
+  end
+end
