879d8890966f363ee2098cba6bfe67142f67c25d
Petr Radouš <darroue@gmail.com>
2024-03-01 14:51:55 +0100

    Add param validations

--- app.rb
+++ app.rb
@@ -10,15 +10,16 @@ module DocBox
 
     resource :qrcode do
       params do
-        requires :iban, type: String, allow_blank: false, regexp: /[A-Z0-9]{15,34}/
-        requires :variable_symbol, type: Integer
-        requires :specific_symbol, type: Integer
-        requires :amount, type: Integer, allow_blank: false
-        requires :message, type: String
-        requires :currency, type: String, default: "CZK", allow_blank: false, regexp: /[A-Z]{3}/
-        requires :payment_date, type: String, regexp: /\d{8}/
-        optional :border, type: Integer, default: 4
-        optional :size, type: Integer, default: 120
+        requires :iban, type: String, allow_blank: false, regexp: /^[A-Z0-9]{15,34}$/
+        requires :variable_symbol, type: Integer, regexp: /^\d{10}$/
+        requires :specific_symbol, type: Integer, regexp: /^\d{10}$/
+        requires :constant_symbol, type: Integer, regexp: /^\d{10}$/
+        requires :amount, type: Integer, allow_blank: false, regexp: /^\d{1,7}(\.\d{1,2}){0,1}$/
+        requires :message, type: String, regexp: /^[A-Z0-9\s]{0,60}$/
+        requires :currency, type: String, default: "CZK", allow_blank: false, regexp: /^[A-Z]{3}$/
+        requires :payment_date, type: Integer, regexp: /^\d{8}$/
+        optional :border, type: Integer, default: 4, regexp: /^\d{1,4}$/
+        optional :size, type: Integer, default: 120, regexp: /^\d{1,4}$/
       end
       post :payment do
         content_type 'image/png'
